6ab62397ae9cd0ce0d845dfc13d466089e98433b
julbautista <julbautista@yahoo.com>
2017-04-10 18:57:31 -0400

    betabinomial model created

--- Model Building.R
+++ Model Building.R
@@ -88,7 +88,7 @@ beep()
 fit_sum2 <- rstan::extract(fit2)
 
 print(fit2, digits = 3)
-
+ggplot()
 #fit2
 par(mfcol = c(1,2),
     mar = c(4,3.9,1.5,3.5))
@@ -140,6 +140,65 @@ points(latewins,
        colMeans(fit_sum2$y_pred_late), pch = 16, col = "grey")
 #abline(0,1, lty = 2, col = "gray")
 
+stanc("betabinomial.stan")$status
+fit3 <- stan("betabinomial.stan",
+             data = list("N","seed1", "regwins","playoffwins","latewins"),
+             iter = 1000, chains = 3)
+beep()
+fit_sum3 <- rstan::extract(fit3)
+
+print(fit3, digits = 3)
+ggplot()
+#fit2
+par(mfcol = c(1,2),
+    mar = c(4,3.9,1.5,3.5))
+plot(colMeans(fit_sum3$y_pred_reg),
+     playoffwins,
+     pch = 16,
+     cex = 0.7,
+     xlab = "Predicted",
+     ylab = "Regular wins",
+     cex.lab = 0.8,
+     xlim = c(0,16),
+     ylim = c(0,16))
+abline(0,1, lty = 2, col = "gray")
+plot(colMeans(fit_sum3$y_pred_late),
+     playoffwins,
+     pch = 16,
+     cex = 0.7,
+     xlab = "Predicted",
+     ylab = "Late wins",
+     cex.lab = 0.8,
+     xlim = c(0,16),
+     ylim = c(0,16))
+abline(0,1, lty = 2, col = "gray")
+
+par(mfcol = c(1,2),
+    mar = c(4,3.9,1.5,3.5))
+plot(regwins,
+     playoffwins,
+     pch = 16,
+     cex = 0.7,
+     ylab = "Playoff Wins",
+     xlab = "Regular win %",
+     cex.lab = 0.8,
+     xlim = c(0,1),
+     ylim = c(0,16))
+#abline(0,1, lty = 2, col = "gray")
+points(regwins,
+       colMeans(fit_sum3$y_pred_reg), pch = 16, col = "grey")
+plot(latewins,
+     playoffwins,
+     pch = 16,
+     cex = 0.7,
+     ylab = "Playoff Wins",
+     xlab = "Late Win %",
+     cex.lab = 0.8,
+     xlim = c(0,1),
+     ylim = c(0,16))
+points(latewins,
+       colMeans(fit_sum3$y_pred_late), pch = 16, col = "grey")
+#abline(0,1, lty = 2, col = "gray")
 
 rstan::extract(fit1)
 #write.csv(final, "playoff data.csv", row.names = F)
---
+++ betabinomial.stan
@@ -0,0 +1,56 @@
+data{
+  int<lower = 0> N;
+  real<lower = 0, upper = 1> regwins[N];
+  // real<lower = 0, upper = 1> playoffwins[N];
+  int playoffwins[N];
+  real<lower = 0, upper = 1> latewins[N];
+  int<lower = 0> seed1[N];
+}
+
+parameters {
+  real beta_reg[8];
+  real beta_late[8];
+  real <lower = 0> sigma_reg;
+  real <lower = 0> sigma_late;
+  real alpha_late[8];
+  real alpha_reg[8];
+  real mu_late;
+  real mu_reg;
+  // real sigma;
+}
+
+transformed parameters {
+  real A_reg[N];
+  real A_late[N];
+  real B_reg[N];
+  real B_late[N];
+  for(i in 1:N){
+    A_late[i] = inv_logit(alpha_late[seed1[i]] + beta_late[seed1[i]]*latewins[i]);
+    A_reg[i] = inv_logit(alpha_reg[seed1[i]] + beta_reg[seed1[i]]*regwins[N]);
+    B_late[i] = inv_logit(alpha_late[seed1[i]] + beta_late[seed1[i]]*latewins[i]);
+    B_reg[i] = inv_logit(alpha_reg[seed1[i]] + beta_reg[seed1[i]]*regwins[N]);
+  }  
+}
+
+
+model {
+  for(i in 1:N){
+    playoffwins[i] ~ beta_binomial(16, A_reg[i], B_reg[i]);
+    playoffwins[i] ~ beta_binomial(16, A_late[i], B_late[i]);      
+  }
+  beta_late ~ normal(mu_late, sigma_late);
+  beta_reg ~ normal(mu_reg, sigma_reg);
+  alpha_late ~ normal(0, 3);
+  alpha_reg ~ normal(0, 3);
+  mu_late ~ normal(0, 10);
+  mu_reg ~ normal(0, 10);
+}
+
+generated quantities{
+  real y_pred_reg[N];
+  real y_pred_late[N];
+  for(i in 1:N){
+    y_pred_reg[i] = beta_binomial_rng(16, A_reg[i], B_reg[i]);
+    y_pred_late[i] = beta_binomial_rng(16, A_late[i], B_late[i]);
+  }
+}
